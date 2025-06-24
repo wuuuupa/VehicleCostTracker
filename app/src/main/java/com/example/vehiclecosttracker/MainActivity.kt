c5faa38a92b874bf231545cd8cb08cd22668c8bf
wuuuupa <sy51188544@gmail.com>
2025-06-24 09:12:25 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/vehiclecosttracker/MainActivity.kt
+++ app/src/main/java/com/example/vehiclecosttracker/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.vehiclecosttracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 通过ID找到按钮
+        val addButton = findViewById<Button>(R.id.button_add_expense)
+
+        // 设置点击事件
+        addButton.setOnClickListener {
+            // 跳转到 AddExpenseActivity
+            val intent = Intent(this, AddExpenseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
