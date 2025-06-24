3d043d413fe5515aad81d6f4011769c3f6818748
wuuuupa <sy51188544@gmail.com>
2025-06-24 09:53:11 +0800

    Update Expense.kt

--- app/src/main/java/com/example/vehiclecosttracker/Expense.kt
+++ app/src/main/java/com/example/vehiclecosttracker/Expense.kt
@@ -1,6 +1,8 @@
 package com.example.vehiclecosttracker
 
 data class Expense(
-    val amount: Double,
-    val note: String
+    val id: Long = 0,           // 数据库主键ID
+    val amount: Double,         // 支出金额
+    val note: String,           // 备注信息
+    val timestamp: Long         // 时间戳（用于排序、显示）
 )
